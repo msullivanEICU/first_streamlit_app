86a2a50f6505d9ee18911e8b1449c208edf2d9eb
msullivanEICU <119885321+msullivanEICU@users.noreply.github.com>
2022-12-05 11:27:45 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,6 @@ streamlit.header("Breakfast Favorites")
 streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
 streamlit.text("🥗 Kale, Spinach, & Rocket Smoothie")
 streamlit.text("🥚 Hard-Boiled Free-Range Egg")
+streamlit.text("🥑🍞 Avocado Toast)
+            
+streamlit.header("🍌🍓 Build Your Own Fruit Smoothie 🍇🥝")              
