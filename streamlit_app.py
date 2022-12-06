da29700d5eb0cd918aa8cb4101f3508caf06624d
msullivanEICU <119885321+msullivanEICU@users.noreply.github.com>
2022-12-06 16:32:40 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,4 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
+streamlit.header("Fruityvice Fruit Advice!")
